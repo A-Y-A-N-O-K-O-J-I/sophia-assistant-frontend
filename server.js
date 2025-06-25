7bcb7cfffc3fefb7bef4e1eea138c656ad362e10
ayanokojix23 <ayanokojix2306@gmail.com>
2025-06-25 14:58:56 +0000

    added others:

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the "auth" folder
-app.use('/auth', express.static(path.join(__dirname, 'auth')));
+app.get("/super-admin/login",(req,res) =>{
+	res.sendFile(path.join(__dirname,"auth","super-admin-login.html"));
+})
+app.get("/super-admin/signup",(req,res) =>{
+	res.sendFile(path.join(__dirname,"auth","super-admin-signup.html"));
+})
+app.get("/admin/login",(req,res) =>{
+	res.sendFile(path.join(__dirname,"auth","admin-login.html"));
+})
+
 
 app.listen(3000, '0.0.0.0', () => {
-  console.log('Running at http://localhost:3000/auth/login.html');
+  console.log('Running at localhost 3000 :)');
 });
